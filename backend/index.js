53c6c27371841ad81fb2bd15c3449f2616235458
Maurice <your.email@example.com>
2024-11-18 23:53:24 +0000

    add/update files

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 
 import connectDB from "./config/db.js";
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -17,8 +18,6 @@ app.use(express.json())
 app.use(express.urlencoded({extended: true}))
 app.use(cookieParser())
 
-app.get("/", (req, res) => {
-    res.send("Hello Word");
-});
+app.use('/api/users', userRoutes);
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
