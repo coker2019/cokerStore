6a9e030c010ecdd61bd6dcb232096fdd20c4491f
Maurice <your.email@example.com>
2024-12-23 11:03:20 +0100

    add Login structure/function

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,10 +1,40 @@
 import express from "express";
-import { createUser,  loginUser } from "../controllers/userController.js";
+import { createUser,  
+    loginUser, 
+    logoutCurrentUser ,
+    getAllUsers,
+    getCurrentUserProfile,
+    updateCurrentUserProfile,
+    deleteUserById,
+    getUserById,
+    updateUserById,
+
+} from "../controllers/userController.js";
+
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router()
 
 
-router.route('/').post(createUser);
-router.post('/auth', loginUser)
+router
+  .route("/")
+  .post(createUser)
+  .get(authenticate, authorizeAdmin, getAllUsers);
+
+  
+router.post("/auth", loginUser);
+router.post("/logout", logoutCurrentUser);
+
+router
+.route('/profile')
+.get(authenticate, getCurrentUserProfile)
+.put(authenticate, updateCurrentUserProfile);
+
+
+//ADMIN ROUTES
+router
+.route('/:id').delete(authenticate, authorizeAdmin, deleteUserById)
+.get(authenticate, authorizeAdmin, getUserById)
+.put(authenticate, authorizeAdmin, updateUserById)
 
 export default router;
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDom from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import {Route, RouterProvider, createRoutesFromElements} from "react-router";
+import { createBrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import  store from "./redux/store.js"
 
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+//Auth
+import Login from "./pages/Auth/Login.jsx";
+
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route path="" element={<Login/>}  />
+  </Route>)
+);
+
+ReactDom.createRoot(document.getElementById("root")).render(
+   <Provider store={store}>
+    <RouterProvider router={router}  />
+    </Provider>
+);
